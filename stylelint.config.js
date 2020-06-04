9167bbdcd758f8b4ce450a21a1bda4a41b129365
Ricard Torres <ricard.torres@netcentric.biz>
2020-06-04 09:21:05 +0200

    Lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,9 +4,6 @@ module.exports = {
   rules: {
     'at-rule-no-unknown': [true, {
       ignoreAtRules: ['function', 'if', 'each', 'else', 'for', 'include', 'mixin', 'return', 'warn']
-    }],
-    'plugin/no-unsupported-browser-features': [true, {
-      ignore: ['css3-cursors', 'outline', 'object-fit', 'css-sticky', 'css-appearance', 'css-mixblendmode', 'word-break', 'user-select-none', 'css-resize', 'viewport-units', 'multicolumn', 'calc', 'rem', 'flexbox', 'css-filters', 'css-hyphens']
     }]
   }
 };
