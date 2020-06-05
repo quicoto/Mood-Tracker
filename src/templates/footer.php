9db81e1761df2278785af2c02ff72cc92917fef4
Ricard Torres <torres.rick@gmail.com>
2020-06-05 22:57:10 +0200

    Improve footer link

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
-  <div class="notifyMe">Please notify me</div>
+  <a class="notifyMe">Allow push notification every day at 21h</a>
   Made with <img src="./assets/icons/028.svg" alt="Love"> by <a href="https://ricard.dev">Ricard Torres</a> <span class="github"><img src="./assets/icons/091.svg" alt="Love"> <a href="https://github.com/quicoto/Mood-Tracker">Github</a></span>
 </footer>
--- src/templates/version.php
+++ src/templates/version.php
@@ -1,1 +1,1 @@
-<?php $version = '2.0.8';
+<?php $version = '2.0.9';
